7e29007f96f76962fc4341b284e8906e282d57c7
pedrosnaves <pedrosnaves@gmail.com>
2025-04-16 18:34:53 -0300

    Update Cadastro.java

--- ex03/Cadastro.java
+++ ex03/Cadastro.java
@@ -1,19 +1,19 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Cadastro {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private ArrayList<Morador> morador;
 
-        ArrayList<Moradores> moradores = new ArrayList<>();
-        moradores.add(new Moradores ("Claudio", "70864285744", "62995874412", "12/10/1956", "masculino", "A", 102, 1565));
-        moradores.add(new Moradores ("Ramon", "40122587466", "62980521466", "14/03/1945", "masculino", "B", 903, 2548));
-        moradores.add(new Moradores ("Jorge", "00125863633", "62980566624", "10/11/1999", "masculino", "B", 801, 3305));
-        moradores.add(new Moradores ("Abadia", "85966410002", "62999568874", "20/12/1975", "feminino", "C", 505, 4023));
-        moradores.add(new Moradores ("Hilda", "11235684715", "62988451965", "19/09/1988", "feminino", "A", 302, 5900));
-
-        for (int i = 0; i < moradores.size(); i++){
-            System.out.println(moradores.get(i));
-        }
+    public Cadastro() {
+        morador = new ArrayList<>();
     }
+
+    public void cadastrarMorador(Morador m) {
+        morador.add(m);
+    }
+
+    public void listarMorador() {
+        for (Morador m : morador) {
+            System.out.println(m);
+        }
+    }    
 }
